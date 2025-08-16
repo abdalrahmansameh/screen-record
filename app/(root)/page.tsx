7792d2f38d9b9b9294f243eff113912c9e037177
abdalrahmansameh <darkness.itec.eg@gmail.com>
2025-08-16 08:07:22 +0300

    Remove unused favicon and default page; update global styles with new theme variables and layout adjustments for improved design consistency.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library"/>
+      <h1 className="text-3xl font-karla">Welcome to loom clone !</h1>
+    </main>
+  );
+}
+
+export default Page
